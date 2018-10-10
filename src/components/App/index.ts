fc21b0b6ae5f99ea0a387822ea1750a1ff2a0542
Leonid Belikov <leonid@belikov.net>
2018-10-10 03:00:57 +0300

    + renderPanel

--- src/components/App/index.ts
+++ src/components/App/index.ts
@@ -1,14 +1,36 @@
 import { setCellSize } from '../../utils/common';
-import { renderEditorBoard } from './render';
+import { renderEditorBoard, renderPanel } from './render';
 
 class App {
   appRoot: HTMLElement;
   editorBoardGrid: HTMLElement;
+  editorPanel: HTMLElement;
+  panelObjects: {
+    ball: HTMLElement;
+    exit: HTMLElement;
+    wall: HTMLElement;
+    stone: HTMLElement;
+    stoneUp: HTMLElement;
+    stoneRight: HTMLElement;
+    stoneDown: HTMLElement;
+    stoneLeft: HTMLElement;
+  }
   cellSize: number;
 
   constructor() {
     this.appRoot = document.getElementById('root');
     this.editorBoardGrid = document.createElement('div');
+    this.editorPanel = document.createElement('div');
+    this.panelObjects = {
+      ball: document.createElement('div'),
+      exit: document.createElement('div'),
+      wall: document.createElement('div'),
+      stone: document.createElement('div'),
+      stoneUp: document.createElement('div'),
+      stoneRight: document.createElement('div'),
+      stoneDown: document.createElement('div'),
+      stoneLeft: document.createElement('div'),
+    };
 
     this.cellSize = setCellSize();
 
@@ -17,6 +39,7 @@ class App {
 
   render() {
     renderEditorBoard.call(this);
+    renderPanel.call(this);
   }
 }
 
--- src/components/App/render.ts
+++ src/components/App/render.ts
@@ -3,17 +3,16 @@
  */
 function renderEditorBoard() {
   const editorBoard: HTMLElement = document.createElement('div');
-  const editorBoardPanel: HTMLElement = document.createElement('div');
 
   this.appRoot.innerHTML = '';
 
   editorBoard.className = 'editorBoard';
   this.editorBoardGrid.className = '-grid';
-  editorBoardPanel.className = '-panel';
+  this.editorPanel.className = '-panel';
 
   this.appRoot.appendChild(editorBoard);
   editorBoard.appendChild(this.editorBoardGrid);
-  editorBoard.appendChild(editorBoardPanel);
+  editorBoard.appendChild(this.editorPanel);
 
   for (let y = 0; y < 20; y += 1) {
     for (let x = 0; x < 32; x += 1) {
@@ -31,4 +30,50 @@ function renderEditorBoard() {
   }
 }
 
-export { renderEditorBoard };
+function renderPanel() {
+  const panelObjects: HTMLElement = document.createElement('div');
+  const panelObjectBallCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelObjectExitCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelObjectWallCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelObjectStoneCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelObjectStoneUpCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelObjectStoneRightCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelObjectStoneDownCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelObjectStoneLeftCanvas: HTMLCanvasElement = document.createElement('canvas');
+  const panelActions: HTMLElement = document.createElement('div');
+  const panelActionGenerate: HTMLElement = document.createElement('div');
+
+  panelObjects.className = '-objects';
+  panelActions.className = '-actions';
+  this.panelObjects.ball.className = '-object';
+  this.panelObjects.exit.className = '-object';
+  this.panelObjects.wall.className = '-object';
+  this.panelObjects.stone.className = '-object';
+  this.panelObjects.stoneUp.className = '-object';
+  this.panelObjects.stoneRight.className = '-object';
+  this.panelObjects.stoneDown.className = '-object';
+  this.panelObjects.stoneLeft.className = '-object';
+  panelActionGenerate.className = '-generate';
+
+  this.editorPanel.appendChild(panelObjects);
+  panelObjects.appendChild(this.panelObjects.ball);
+  this.panelObjects.ball.appendChild(panelObjectBallCanvas);
+  this.panelObjects.exit.appendChild(panelObjectExitCanvas);
+  this.panelObjects.wall.appendChild(panelObjectWallCanvas);
+  this.panelObjects.stone.appendChild(panelObjectStoneCanvas);
+  this.panelObjects.stoneUp.appendChild(panelObjectStoneUpCanvas);
+  this.panelObjects.stoneRight.appendChild(panelObjectStoneRightCanvas);
+  this.panelObjects.stoneDown.appendChild(panelObjectStoneDownCanvas);
+  this.panelObjects.stoneLeft.appendChild(panelObjectStoneLeftCanvas);
+  panelObjects.appendChild(this.panelObjects.exit);
+  panelObjects.appendChild(this.panelObjects.wall);
+  panelObjects.appendChild(this.panelObjects.stone);
+  panelObjects.appendChild(this.panelObjects.stoneUp);
+  panelObjects.appendChild(this.panelObjects.stoneRight);
+  panelObjects.appendChild(this.panelObjects.stoneDown);
+  panelObjects.appendChild(this.panelObjects.stoneLeft);
+  this.editorPanel.appendChild(panelActions);
+  panelActions.appendChild(panelActionGenerate);
+}
+
+export { renderEditorBoard, renderPanel };
