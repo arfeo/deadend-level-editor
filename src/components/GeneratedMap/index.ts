47776e9e1577502f6876f41b32c968230adcdb3e
Leonid Belikov <mail@arfeo.net>
2018-10-15 02:42:04 +0300

    GeneratedMap modal component

--- src/components/GeneratedMap/index.ts
+++ src/components/GeneratedMap/index.ts
@@ -2,18 +2,33 @@ import { Modal } from '../common/Modal';
 import { App } from '../App';
 
 class GeneratedMap extends Modal {
+  mapTextArea: HTMLTextAreaElement;
+
   constructor(app: App) {
-    super(app, 'large');
+    super(app);
   }
 
   render() {
-    const textContainer: HTMLElement = document.createElement('div');
+    const generatedMapContainer: HTMLElement = document.createElement('div');
+    const paragraph: HTMLParagraphElement = document.createElement('p');
+    this.mapTextArea = document.createElement('textarea');
+    const copyToClipboardButton: HTMLButtonElement = document.createElement('button');
 
-    textContainer.innerHTML = (`
-      Hello world.
-    `);
+    generatedMapContainer.innerHTML = '<strong>Generated map</strong>';
+    this.mapTextArea.innerHTML = JSON.stringify(this.app.currentMap);
+    this.mapTextArea.style.width = '100%';
+    this.mapTextArea.style.height = '40vmin';
+    copyToClipboardButton.innerHTML = 'Copy to clipboard';
 
-    this.modal.appendChild(textContainer);
+    this.modal.appendChild(generatedMapContainer);
+    generatedMapContainer.appendChild(paragraph);
+    generatedMapContainer.appendChild(copyToClipboardButton);
+    paragraph.appendChild(this.mapTextArea);
+
+    copyToClipboardButton.addEventListener('click', () => {
+      this.mapTextArea.select();
+      document.execCommand('copy');
+    });
   }
 }
 
